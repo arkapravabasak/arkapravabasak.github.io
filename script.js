76e8444d35191b9526c516c049c3eb947178bec2
Arkaprava Basak <arkaprava.basak@outlook.com>
2026-03-12 20:58:56 +0530

    add header section

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function toggleTheme() {
     }
 
 }
+
+fetch("/header.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("header-placeholder").innerHTML = data;
+});
