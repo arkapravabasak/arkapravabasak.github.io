320b476e6eba644766fba0105e388818f64549cb
Arkaprava Basak <arkaprava.basak@outlook.com>
2026-03-12 17:21:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function toggleTheme() {
     body.classList.toggle("dark-mode");
 
     if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
         btn.innerText = "☀ Light Mode";
     } else {
+        localStorage.setItem("theme", "light");
         btn.innerText = "🌙 Dark Mode";
     }
 
