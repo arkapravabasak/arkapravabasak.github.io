cc616af8495a929bfe196f7dc62bd63bf54cdce9
Arkaprava Basak <arkaprava.basak@outlook.com>
2026-03-14 15:42:39 +0530

    add expertise write up modal popup card

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ document.getElementById("footer").innerHTML = data;
 });
 
 });
+
+function openModal(modalId){
+
+document.getElementById(modalId).style.display="flex";
+
+}
+
+function closeModal(modalId){
+
+document.getElementById(modalId).style.display="none";
+
+}
+
+window.onclick = function(event){
+
+const modals = document.querySelectorAll(".modal");
+
+modals.forEach(function(modal){
+
+if(event.target === modal){
+modal.style.display="none";
+}
+
+});
+
+};
