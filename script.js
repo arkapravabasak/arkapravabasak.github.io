d3ed38921f986cfcee3caad01bf253dd4b4f2e8f
Arkaprava Basak <arkaprava.basak@outlook.com>
2026-03-22 23:07:50 +0530

    modal fix

--- script.js
+++ script.js
@@ -23,30 +23,23 @@ fetch(basePath + "footer.html")
 });
 
 function openModal(modalId){
-
 document.getElementById(modalId).style.display="flex";
-
 }
 
 function closeModal(modalId){
-
 document.getElementById(modalId).style.display="none";
-
 }
 
 window.onclick = function(event){
-
-const modals = document.querySelectorAll(".modal");
-
-modals.forEach(function(modal){
-
-if(event.target === modal){
-modal.style.display="none";
-}
-
-});
-
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(function(modal){
+        if(event.target === modal){
+        modal.style.display="none";
+        }
+    });
 };
+
+
 
 function openImageModal(src){
 document.getElementById("imageModal").style.display="flex";
